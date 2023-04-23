e8907ab3b4e5028e06d1571a3b1f7db18fafe1a1
MEKA <41925947+mekalix@users.noreply.github.com>
2023-04-23 22:22:25 +0200

    Update

--- Survivors Clone/Utility/hurt_box.gd
+++ Survivors Clone/Utility/hurt_box.gd
@@ -5,7 +5,7 @@ extends Area2D
 @onready var collision = $CollisionShape2D
 @onready var disableTimer = $DisableTimer
 
-signal hurt(damage)
+signal hurt(damage, angle, knockback)
 
 func _on_area_entered(area):
 	if area.is_in_group("attack"):
@@ -20,7 +20,14 @@ func _on_area_entered(area):
 					if area.has_method("tempdisable"):
 						area.tempdisable()
 			var damage = area.damage
-			emit_signal("hurt", damage)
+			var angle = Vector2.ZERO
+			var knockback = 1
+			if not area.get("angle") == null:
+				angle = area.angle
+				if not area.get("knockback_amount") == null:
+					knockback = area.knockback_amount
+			
+			emit_signal("hurt", damage, angle, knockback)
 			if area.has_method("enemy_hit"):
 				area.enemy_hit(1)
 
